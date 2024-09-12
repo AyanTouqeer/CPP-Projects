5b952f5ebee0574b7b16cafcd3923c44fbc7dab5
AyanTouqeer <145884276+AyanTouqeer@users.noreply.github.com>
2024-09-12 10:43:36 +0500

    courseInfoCGPAcal.cpp

--- singleStudentCGPAcal.cpp
+++ singleStudentCGPAcal.cpp
@@ -1,59 +1,73 @@
+//CGPA calculator based on Course information.
+
 #include <iostream>
 #include <string>
-#include <iomanip>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
+//Structure for course attributes.
 struct MyCoursesAttributes{
     string courseName;
     double creditHours;
     double gradePoints;
 };
 
-
-class MyCourse{
+//My course class with only one method to calculate cgpa.
+class MyCourses{
 public:
-    double calculateCGPA(const vector<MyCoursesAttributes>& courses){ //taking a parameter of a vector.
-        double totalCreditHours = 0.0;
-        double totalGradePoints = 0.0;
 
-        //using a range based for loop.
-        for(const MyCoursesAttributes& course : courses){ // for loop will iterate over every element in the vector.
-            totalCreditHours += course.creditHours; // credit hours of current course will be added.
-            totalGradePoints += course.creditHours * course.gradePoints;
+    //A method to calculate cgpa.
+    double calculateCGPA(const vector<MyCoursesAttributes>& courses){
+        double totalCreditHours = 0.0;
+        double totalGradePoints =  0.0;
+
+        //a range based for loop to iterate over vector of type MyCourseAttributes.
+        for (const MyCoursesAttributes& course : courses){
+            totalCreditHours += course.creditHours;
+            totalGradePoints += course.gradePoints * course.creditHours;
         }
 
-        //values are incremented in the variables. Therefore, now return there division.
+        //return the value of cgpa.
         return totalGradePoints / totalCreditHours;
     }
-
+    
 };
 
-
 int main(){
+    //To store num of courses enter by the user.
     int numCourses;
-    cout << "Enter number of courses : " << endl;
-    cin >> numCourses;
-    //create a class obj.
-    MyCourse MyCourseObj;
-    //defining a vector.
-    vector<MyCoursesAttributes> courses(numCourses); // courses vector has been created.
 
-    //for loop to assign value to attributes of elements in a vector.
-    for (int i = 0; i < numCourses ; ++i){
-        cout << "Course no. " << i + 1 << endl;
-        cout << "Enter course name : " << endl;
+    //Prompting the user to enter num of courses.
+    cout <<"Enter number of courses : " << endl;
+    cin >> numCourses;
+
+    //A vector of courses to passed as an argument when calling the function.
+    vector<MyCoursesAttributes> courses(numCourses);
+
+    //Using a for loop to assign values given by user to vector elements.
+    for (int i = 0; i < numCourses; i++){
+        cout << "Course no. : " << i + 1 << endl;
+        cout << "Enter Course Name : " << endl;
         cin >> courses[i].courseName;
-        cout << "Enter credit hours : " << endl;
+        cout << "Enter Credit Hours : " << endl;
         cin >> courses[i].creditHours;
-        cout << "Enter grade points : " << endl;
+        cout << "Enter Grade Points Obtained : " << endl;
         cin >> courses[i].gradePoints;
     }
 
-    double cgpa = MyCourseObj.calculateCGPA(courses); // This line is highlighted by a red line.
+    //Obj of class to access method.
+    MyCourses MyCoursesObj;
 
-    cout << "\nYour CGPA is : " << fixed << setprecision(2) << cgpa << endl;
+    //calling method and storing its value in a variable.
+    double cgpa;
+    cgpa = MyCoursesObj.calculateCGPA(courses);
+
+    //Display cgpa.
+    cout << "\nYou obtained a CGPA of : " << fixed << setprecision(2) << cgpa << endl;
+
+    return 0;
 }
 
 
